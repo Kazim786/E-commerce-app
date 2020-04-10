511c3ddbbb30682b041ab36934f21ca02bcdc60c
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-10 13:59:44 -0500

    imported react router dom '

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage/homepage.component'
 
-function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
