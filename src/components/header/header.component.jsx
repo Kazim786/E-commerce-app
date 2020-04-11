cff5574a003ae97f6515d25e499f79e9c43f4c13
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-11 15:33:40 -0500

    exported header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,18 @@ import './header.style.scss'
 import {ReactComponent as Logo} from '/Users/apple/E-Commerce/king-clothing/src/assets/648px-Flag_of_the_Pakistani_Army.svg.png'
 const Header = () => (
     <div className= 'header'>
-        <Link to= "/">
-
-
+        <Link className= 'logo-container' to= "/">
+            <Logo className= 'logo' />
         </Link>
+        <div className= 'options'>
+        <Link className= 'option' to= "/shop">
+            SHOP 
+        </Link>
+        <Link className= 'option' to= "/shop">
+            CONTACT
+        </Link>
+        </div>
     </div>
 )
+
+export default Header 
