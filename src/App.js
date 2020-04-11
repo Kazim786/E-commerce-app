14f523aea3c747fea3b18144702055fd93c50b1e
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-11 13:04:17 -0500

    added withRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+
+)
+  
+  function App () {
+    
+      return (
+        <div>
+          <Switch>
+          <Route exact path = '/' component={HomePage} />
+          <Route exact path = '/hats' component={HatsPage} />
+          </Switch>
+        </div>
+      );
   }
-}
+
+
+
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <HomePage />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
