6350d9d5a5b9620c4b7d8893e8cf091057f41630
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-11 15:38:58 -0500

    added the crown logo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import './header.style.scss'
-import {ReactComponent as Logo} from '/Users/apple/E-Commerce/king-clothing/src/assets/648px-Flag_of_the_Pakistani_Army.svg.png'
+import {ReactComponent as Logo} from '/Users/apple/E-Commerce/king-clothing/src/assets/king.svg'
 const Header = () => (
     <div className= 'header'>
         <Link className= 'logo-container' to= "/">
