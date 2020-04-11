3ed66b23cd25670ce4d69408674d87167cf30935
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-11 15:32:18 -0500

    added flag

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import './header.style.scss'
+import {ReactComponent as Logo} from '/Users/apple/E-Commerce/king-clothing/src/assets/648px-Flag_of_the_Pakistani_Army.svg.png'
+const Header = () => (
+    <div className= 'header'>
+        <Link to= "/">
+
+
+        </Link>
+    </div>
+)
