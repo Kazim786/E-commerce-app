7d80a172c71f8191824f9690b26f3e5e69e82373
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-12 14:59:37 -0500

    connected database

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -18,7 +18,23 @@ const config = {
 
         const userRef = firestore.doc(`users/${userAuth.uid}`)
         const snapShot = await userRef.get()
-        console.log(snapShot)
+        if(!snapShot.exists){
+            const {displayName, email} = userAuth;
+            const createdAt = new Date();
+
+            try{
+                await userRef.set({
+                    displayName,
+                    email,
+                    createdAt,
+                    ...additionalData
+                })
+            }
+            catch (error) {
+                console.log('error creating user', error.message);
+            }
+        }
+        return userRef;
     };
 
     firebase.initializeApp(config);
