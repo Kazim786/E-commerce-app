a7436bfe08adb76792deafb3b4f519b6a5172cda
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-12 07:47:48 -0500

    imported custom button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import FormInput from '../form-input/form-input.component'
-
+import CustomButton from '../custom-button/custom-button.component.jsx'
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
