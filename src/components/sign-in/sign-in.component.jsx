6da9be31e033a67bee872446c7619242dd18c158
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-12 08:30:27 -0500

    created sign in with google:

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component.jsx'
+import { signInWithGoogle } from '../../firebase/firebase.util'
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -45,6 +46,7 @@ class SignIn extends React.Component{
                         required />
                     
                     <CustomButton type= 'submit'> Sign In </CustomButton>
+                    <CustomButton onClick= { signInWithGoogle }> Sign In With Google </CustomButton>
                 </form>
             </div>
         )
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,1 +1,25 @@
 import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDM5-8RUmFCkWSn-jb1-OJNuSZkK79cQV0",
+    authDomain: "king-clothing-b0cea.firebaseapp.com",
+    databaseURL: "https://king-clothing-b0cea.firebaseio.com",
+    projectId: "king-clothing-b0cea",
+    storageBucket: "king-clothing-b0cea.appspot.com",
+    messagingSenderId: "371709961541",
+    appId: "1:371709961541:web:9e940d9eb17f3d6ec88a2c",
+    measurementId: "G-8SQGKZMNCT"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ promp: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
