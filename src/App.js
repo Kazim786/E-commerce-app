980ad56238b40ccd6a27ebe32ac52c8735e30c2a
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-12 08:42:24 -0500

    converted function into class

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out-page/sign-in-and-sign-out-page.component'
 import Header from './components/header/header.component'
+import { auth } from './firebase/firebase.util';
 
 
 // const HatsPage = () => (
@@ -14,18 +15,33 @@ import Header from './components/header/header.component'
 
 // )
   
-  function App () {
-    
-      return (
-        <div>
-          <Header />
-          <Switch>
-          <Route exact path = '/' component={HomePage} />
-          <Route exact path = '/shop' component={ShopPage} />
-          <Route exact path = '/signin' component={SignInAndSignUpPage} />
-          </Switch>
-        </div>
-      );
+  class  App extends React.Component () {
+    constructor(){
+      super();
+
+      this.state = {
+        currentUser: null
+      }
+    }
+
+    componentDidMount(){
+      auth.onAuthStateChanged(user => {
+        this.setState({ currentUser: user });
+      }
+    }
+
+      render(){
+        return (
+          <div>
+            <Header />
+            <Switch>
+            <Route exact path = '/' component={HomePage} />
+            <Route exact path = '/shop' component={ShopPage} />
+            <Route exact path = '/signin' component={SignInAndSignUpPage} />
+            </Switch>
+          </div>
+        );
+      }
   }
 
 
