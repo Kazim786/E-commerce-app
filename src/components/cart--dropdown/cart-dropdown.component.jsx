903b77b04ccef5a291546c728d97f8a907873a49
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-13 10:50:59 -0500

    imported withRouter and then made checkout component

--- src/components/cart--dropdown/cart-dropdown.component.jsx
+++ src/components/cart--dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselects'
+import { withRouter } from 'react-router-dom'
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({cartItems, history }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'> 
     {cartItems.length ? (
@@ -18,10 +19,13 @@ const CartDropdown = ({cartItems}) => (
       )}
     
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick= {() => history.push('/checkout')}>
+      GO TO CHECKOUT
+      </CustomButton>
   </div>
 );
 const mapStateToProps = createStructuredSelector ({
   cartItems: selectCartItems
 })
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
+//withRouter will pass the match history etc
