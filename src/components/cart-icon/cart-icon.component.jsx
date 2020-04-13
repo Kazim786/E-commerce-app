bc5f4b1ad984fb34a341039a0792ab56d5ad1582
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-12 21:26:22 -0500

    added toggle feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { toggleCartHidden } from '../../redux/cart/cart.action'
 import { ReactComponent as ShoppingIcon } from '/Users/apple/E-Commerce/king-clothing/src/assets/carrt.svg'
 
 import './cart-icon.styles.scss';
 
 const CartIcon = () => (
-    <div className= 'cart-icon'>
+    <div className= 'cart-icon' onClick= {toggleCartHidden}>
         <ShoppingIcon className= 'shopping-icon' />
         <span className= 'item-count'>0</span>
     </div>
 );
 
-export default CartIcon
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(
+    null,
+    mapDispatchToProps)
+    (CartIcon)
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,7 @@ import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropDown from '../cart--dropdown/cart-dropdown.component'
 import './header.style.scss'
 import {ReactComponent as Logo} from '/Users/apple/E-Commerce/king-clothing/src/assets/king.svg'
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className= 'header'>
         <Link className= 'logo-container' to= "/">
             <Logo className= 'logo' />
@@ -26,12 +26,14 @@ const Header = ({ currentUser }) => (
 )}
         <CartIcon />
         </div>
-        <CartDropDown />
+        {hidden ? null :
+        <CartDropDown />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden 
 })
 
 
