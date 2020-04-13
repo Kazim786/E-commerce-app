809d5026b38fd2eab772855631a6ecc4bd119aee
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-13 10:29:51 -0500

    restructured code

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
 
 import { auth } from '/Users/apple/E-Commerce/king-clothing/src/firebase/firebase.util.js;
 import CartIcon from '/Users/apple/E-Commerce/king-clothing/src/assets/carrt.svg';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
+import { selectCurrentUser } from '../../redux/user/user.selector'
 import { ReactComponent as Logo } from '/Users/apple/E-Commerce/king-clothing/src/assets/king.svg';
 
 import './header.styles.scss';
@@ -37,9 +39,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser : selectCurrentUser(state),
+  hidden: selectCartHidden(state)
 });
 
 export default connect(mapStateToProps)(Header);
