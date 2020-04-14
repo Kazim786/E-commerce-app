360c50067266b6677ea2ffe20284437f747c51e5
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-14 14:04:00 -0500

    fixed the error

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import {createStructuredSelect} from 'reselect'
+import {createStructuredSelector} from 'reselect'
 import CollectionPreview from '/Users/apple/E-Commerce/king-clothing/src/components/preview-component/collection-preview.component.jsx';
 import {selectCollections} from '../../redux/shop-redux/shop.selectors'
 const ShopPage = ({ collections }) => (
