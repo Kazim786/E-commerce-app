c525344cb8a5d0168d2ba87739a21e31a5ca2f75
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-15 11:48:54 -0500

    app crashed so I fixed the file path and the errors

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 import CollectionsOverview from '/Users/apple/E-Commerce/king-clothing/src/components/collections-overview/collections-overview.component.jsx'
-import CollectionPage from '../collection/collection.component'
+import CollectionPage from '/Users/apple/E-Commerce/king-clothing/src/pages/collections/collection.component.jsx'
 
 const ShopPage = ({match}) => (
       <div className='shop-page'>
           <Route exact path={`${match.path}`} component = {CollectionsOverview} />
-          <Route path= {`${math.path}/:collectionId`} component= {CollectionPage} />
+          <Route path= {`${match.path}/:collectionId`} component= {CollectionPage} />
       </div>
     );
 
