be8485e259867a1407346937bce3cc7a6ac24a33
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-16 11:29:59 -0500

    added the cover pictures

--- src/redux/directory-redux/directory.reducer.js
+++ src/redux/directory-redux/directory.reducer.js
@@ -3,7 +3,7 @@ const INITIAL_STATE = {
     [
         {
             title: 'hats',
-            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            imageUrl: 'https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/06/Man-wearing-kufi-hat.jpg',
             id: 1,
             linkUrl: 'shop/hats'
         },
@@ -14,21 +14,21 @@ const INITIAL_STATE = {
             linkUrl: 'shop/jackets'
         },
         {
-            title: 'sneakers',
+            title: 'shoes',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
             id: 3,
             linkUrl: 'shop/sneakers'
         },
         {
             title: 'womens',
-            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTWnHn5k5u64xU3DBXX8r0Fp-tRSzGbjwCP0669Co_2ziaixpDx&usqp=CAU',
             size: 'large',
             id: 4,
             linkUrl: 'shop/womens'
         },
         {
             title: 'mens',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            imageUrl: 'https://i.pinimg.com/originals/89/ed/44/89ed44367afdb60772ed1adf956648a4.jpg',
             size: 'large',
             id: 5,
             linkUrl: 'shop/mens'
